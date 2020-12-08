382b9a39e6fd2e200e2e3256804e7e29c455d9b0
DimitriNakos <dimi1nakos@gmail.com>
2020-12-07 23:54:25 -0500

    added some logic working through some basic

--- employees.sql
+++ employees.sql
@@ -3,35 +3,44 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- * **department**:
-CREATE TABLE department (
---   * **id** - INT PRIMARY KEY
+CREATE TABLE departments (
 id INT AUTO_INCREMENT NOT NULL,
---   * **name** - VARCHAR(30) to hold department name
-name VARCHAR(30)
+name VARCHAR(30),
+PRIMARY KEY (id)
 );
 
 -- * **role**:
-CREATE TABLE role (
---   * **id** - INT PRIMARY KEY
+CREATE TABLE roles (
 id INT AUTO_INCREMENT NOT NULL,
---   * **title** -  VARCHAR(30) to hold role title
 title VARCHAR(30),
---   * **salary** -  DECIMAL to hold role salary
 salary DECIMAL(10,2),
---   * **department_id** -  INT to hold reference to department role belongs to
-department_id INT(10)
+department_id INT(10),
+PRIMARY KEY (id)
 );
 
 -- * **employee**:
-CREATE TABLE employee (
---   * **id** - INT PRIMARY KEY
+CREATE TABLE employees (
 id INT AUTO_INCREMENT NOT NULL,
---   * **first_name** - VARCHAR(30) to hold employee first name
-first_name VARCHAR(30)
---   * **last_name** - VARCHAR(30) to hold employee last name
-last_name VARCHAR(30)
---   * **role_id** - INT to hold reference to role employee has
-role_id INT(10)
---   * **manager_id** - INT to hold reference to another employee that manages the 
-manager_id INT(10)
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT(10),
+manager_id INT(10),
+PRIMARY KEY (id)
 );
+
+INSERT INTO departments (name)
+VALUES ("Service"), ("Sales"), ("Parts");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("Ronnie", 34.00, 1),
+("Samson", 22.00, 1),
+("JJ", 20.00, 2),
+("Justin", 22.00, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+("Ronnie", "Taylor", 34.00, 1),
+("Samson", "Vang", 22.00, 1),
+("JJ", 20.00, 2),
+("Justin", 22.00, 3);
---
+++ index.js
@@ -0,0 +1,198 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const fs = require('fs');
+const { allowedNodeEnvironmentFlags } = require('process');
+const table = require('console.table');
+
+// const employees_js = require('./employees.js')
+
+// ---------------------------------------------
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM employees, roles, departments", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+};
+// ----------------------------
+
+const options =
+    [{
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'option',
+        choices: [
+            "Add Department",
+            "Add Roles",
+            "Add Employee",
+            "View Departments",
+            "View Roles",
+            "View Employees",
+            "Update Employee Roles"
+        ]
+    }];
+
+// function to initialize program
+function init() {
+    inquirer.prompt(options).then(data => {
+        let choice = data.option
+        switch (choice) {
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Roles':
+                addRoles();
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'View Departments':
+                viewDepartments();
+                break;
+            case 'View Roles':
+                viewRoles();
+                break;
+            case 'View Employees':
+                viewEmployees();
+                break;
+            case 'Update Employees Roles':
+                updateEmployeeRoles();
+                break;
+        }
+    });
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department you would like to add?',
+            name: 'department'
+        }
+    ])
+        .then(answer => {
+            console.log(`Adding ${answer.department} department...\n`);
+            connection.query(
+                "INSERT INTO departments SET ?",
+                {
+                    name: answer.department
+                }, function (err, res) {
+                    if (err) throw err;
+                    connection.end()
+                    res.console.log("department added!\n");
+                }
+            )
+            console.log(query.sql)
+        })
+}
+
+const addRoles = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of the role to add?',
+            name: 'role'
+        },
+        {
+            type: 'input',
+            message: 'What is the salary of the added? (example...  "15.00"',
+            name: 'salary'
+        },
+        {
+            type: 'list',
+            message: 'Which Department do they belong to?',
+            name: 'dept',
+            choices: ["Sales", "Service", "Parts"]
+        }
+    ])
+        .then(answer => {
+
+            switch (answer.dept) {
+                case 'Sales':
+                    answer.dept = 1;
+                    break;
+                case 'Service':
+                    answer.dept = 2;
+                    break;
+                case 'Parts':
+                    answer.dept = 3;
+                    break;
+            }
+
+            console.log(`Adding ${answer.role} role...\n`);
+
+            connection.query(
+                "INSERT INTO roles SET ?",
+                {
+                    title: answer.role,
+                    salary: answer.salary,
+                    department_id: answer.dept
+                }, function (err, res) {
+                    if (err) throw err;
+                    connection.end()
+                    res.console.log("Role added!\n");
+                }
+            )
+            console.log(query.sql)
+        })
+}
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the employees first name?',
+                name: 'empFirstName'
+            }
+        ])
+        .then(answer => {
+            console.log(`Adding ${answer.department} department...\n`);
+            let query = connection.query(
+                "INSERT INTO departments SET ?",
+                {
+                    name: answer.department
+                }, function (err, res) {
+                    if (err) throw err;
+                    connection.end()
+                    res.console.log("department added!\n");
+                }
+            )
+            console.log(query.sql)
+        })
+}
+
+const viewDepartments = () => {
+    console.log("yo")
+
+}
+
+const viewRoles = () => {
+    console.log("yo")
+
+}
+
+const viewEmployees = () => {
+    console.log("yo")
+
+}
+
+const updateEmployeeRoles = () => {
+    console.log("yo")
+
+}
+
+init();
